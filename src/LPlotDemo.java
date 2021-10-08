cd5ef1ecc8849e3972c3d4156fc9678b3d09631d
Tyrone Gallardo <tgall019@fiu.edu>
2021-10-08 19:13:37 -0400

    Code now filters data and creates the grid data structures

--- src/LPlotDemo.java
+++ src/LPlotDemo.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LPlotDemo {
-
+	private static ArrayList<Node[][]> grids = new ArrayList<>();
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner input;
 		try {
@@ -14,8 +16,20 @@ public class LPlotDemo {
 			throw(e);
 		}
 		
+		//filter input and create data structure
+		int gridCount = input.nextInt();
+		for(int k = 0; k < gridCount; k++) {
+			int rows = input.nextInt(), cols = input.nextInt();
+			Node[][] grid = new Node[rows][cols];
+			for(int i = 0; i < rows; i++) {
+				for(int j = 0; j < cols; j++) {
+					grid[i][j] = new Node(i, j, input.nextInt());
+				}
+			}
+			grids.add(grid);
+		}
 		
-		System.out.println(input.nextInt());
+		
 		input.close();
 	}
 
