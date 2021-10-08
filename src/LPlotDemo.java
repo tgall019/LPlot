1e0fbd20d79caa53f3045413e3a942edfeee5fcb
Tyrone Gallardo <tgall019@fiu.edu>
2021-10-08 18:54:00 -0400

    Created input file and added interface for reading input

--- src/LPlotDemo.java
+++ src/LPlotDemo.java
@@ -1,8 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class LPlotDemo {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner input;
+		try {
+			File inputFile = new File("input.txt");
+			input = new Scanner(inputFile);
+		}
+		catch(FileNotFoundException e) {
+			throw(e);
+		}
+		
+		
+		System.out.println(input.nextInt());
+		input.close();
 	}
 
 }
