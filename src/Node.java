1983bd2d565f933d2ff459b7c874c98fda0bca3b
Tyrone Gallardo <tgall019@fiu.edu>
2021-10-08 18:43:41 -0400

    Created custom Node class

---
+++ src/Node.java
@@ -0,0 +1,12 @@
+
+public class Node {
+	public int row, col;
+	public int value;
+	public boolean isEndNode = false;
+	
+	public Node(int r, int c, int v) {
+		this.row = r;
+		this.col = c;
+		this.value = v;
+	}
+}
